1ef800c09aba1eaebe3641327e1a552a0021d99d
Ilya <nechesanov2000@gmail.com>
2023-05-12 20:20:24 +0300

    main

--- app/migrations/0001_initial.py
+++ app/migrations/0001_initial.py
@@ -17,6 +17,7 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('full_name', models.CharField(max_length=250)),
                 ('phone_number', models.CharField(max_length=20)),
+                ('description', models.CharField(max_length=300)),
             ],
         ),
     ]
